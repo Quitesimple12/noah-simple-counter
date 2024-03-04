71f42ba5ff1bc368234df1fd32a54c0c1f9d6f15
Quitesimple12 <122361337+Quitesimple12@users.noreply.github.com>
2024-03-04 00:19:28 +0000

    First commit

---
+++ src/js/component/counter.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function SmallCard() {
+    <div className="card text-center bg-secondary">
+        <div className="card-body">
+            Hello
+        </div>
+    </div>
+}
+
+export {SmallCard}
+
+function Background() {
+    return <div className="card text-center bg-dark" style = {{ height: 150 }}>
+    <div className="card-body">
+        <div className="row">
+        </div>
+
+        </div>
+    </div>
+}
+
+
+
+
+export { Background }
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Timer() {
+  const [timer1, setTimer1] = useState(0);
+  const [timer2, setTimer2] = useState(0);
+  const [timer3, setTimer3] = useState(0);
+  const [timer4, setTimer4] = useState(0);
+  const [timer5, setTimer5] = useState(0);
+  const [timer6, setTimer6] = useState(0);
 
-//create your first component
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer1(prevSlot => (prevSlot + 1) % 10);
+      if (timer1 === 9) {
+        setTimer2(prevSlot => (prevSlot + 1) % 10);
+        if (timer2 === 9) {
+          setTimer3(prevSlot => (prevSlot + 1) % 10);
+          if (timer3 === 9) {
+            setTimer4(prevSlot => (prevSlot + 1) % 10);
+            if (timer4 === 9) {
+              setTimer5(prevSlot => (prevSlot + 1) % 10);
+              if (timer5 === 9) {
+                setTimer6(prevSlot => (prevSlot + 1) % 10);
+              }
+            }
+          }
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [timer1, timer2, timer3, timer4, timer5, timer6]);
+
+  // Wrap the JSX inside a container element and return it
+  return (
+    <div className="row">
+      <div className="col">
+        <SmallCard counter={timer6} />
+      </div>
+      <div className="col">
+        <SmallCard counter={timer5} />
+      </div>
+      <div className="col">
+        <SmallCard counter={timer4} />
+      </div>
+      <div className="col">
+        <SmallCard counter={timer3} />
+      </div>
+      <div className="col">
+        <SmallCard counter={timer2} />
+      </div>
+      <div className="col">
+        <SmallCard counter={timer1} />
+      </div>
+    </div>
+  );
+}
+
+function SmallCard({ counter }) {
+  return (
+    <div className="card text-center bg-secondary" style={{ width: 100, height: 120 }}>
+      <div className="card-body" style={{fontSize: "75px"}}>{counter}</div>
+    </div>
+  );
+}
+
+function Background() {
+  return (
+    <div className="card text-center bg-dark" style={{ height: 150 }}>
+      <div className="card-body">
+        <Timer />
+      </div>
+    </div>
+  );
+}
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div>
+      <Background />
+    </div>
+  );
 };
 
 export default Home;
